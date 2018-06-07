799f43f860d98b3f97fa8deab8530ae6ec98c039
Romain <romain@fifty-five.com>
2018-06-07 14:25:57 +0200

    add paramMap

---
+++ RecommenderSystem/param.py
@@ -0,0 +1,97 @@
+class recoParamMap(object):
+
+	def addOrUpdate(self,name,param):
+		if param.parent is None:
+			setattr(self,name,param)
+		else:
+			try:
+				parent = getattr(self,param.parent)
+			except:
+				setattr(self,param.parent,{})
+				parent = getattr(self,param.parent)
+			parent[param.name] = {'desc':param.desc,'value':param.value}
+
+	def get(self,name,parent=None):
+		if parent is None:
+			out = getattr(self,name)
+			if type(out)==dict:
+				tmp = {}
+				for key in out.keys():
+					tmp[key] = out[key]['value']
+				out = tmp
+			else:
+				out = out.value
+		else:
+			out = getattr(self,parent)
+			out = out[name]['value']
+		return out
+
+class recoParam(object):
+
+	def __init__(self,parent,name,desc,value=None):
+		setattr(self,'parent',parent)
+		setattr(self,'name',name)
+		setattr(self,'desc',desc)
+		setattr(self,'value',value)
+
+class HasLbda(recoParam):
+
+	def __init__(self,paramMap):
+		super(HasLbda,self).__init__(None,'lbda','Tikhonov regularization weight',0)
+		self.paramMap = paramMap
+		self.paramMap.addOrUpdate('lbda',self)
+
+	def setLbda(self,value):
+		self.value = value
+		self.paramMap.addOrUpdate('lbda',self)
+
+	def getLbda(self):
+		return self.value
+
+class HasAlpha(recoParam):
+
+	def __init__(self,paramMap):
+		super(HasAlpha,self).__init__(None,'alpha','implicit weight',0)
+		self.paramMap = paramMap
+		self.paramMap.addOrUpdate('alpha',self)
+
+	def setAlpha(self,value):
+		self.value = value
+		self.paramMap.addOrUpdate('alpha',self)
+
+	def getAlpha(self):
+		return self.value
+
+class HasRank(recoParam):
+
+	def __init__(self,paramMap,name,parent='ranks'):
+		super(HasRank,self).__init__(parent,name,'rank value of %name dimension',1)
+		self.paramMap = paramMap
+		if parent is None:
+			paramMap.addOrUpdate(name,self)
+		else:
+			paramMap.addOrUpdate(parent,self)
+
+	def setRank(self,value):
+		self.value = value
+		if self.parent is None:
+			self.paramMap.addOrUpdate(self.name,self)
+		else:
+			self.paramMap.addOrUpdate(self.parent,self)
+	
+	def getRank(self):
+		return self.value
+
+class HasMaxIter(recoParam):
+
+	def __init__(self,paramMap):
+		super(HasMaxIter,self).__init__(None,'maxIter','maximum number of iterations',1)
+		self.paramMap = paramMap
+		self.paramMap.addOrUpdate('maxIter',self)
+
+	def setMaxIter(self,value):
+		self.value = value
+		self.paramMap.addOrUpdate('maxIter',self)
+
+	def getMaxIter(self):
+		return self.value
